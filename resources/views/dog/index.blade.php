fe65ebfb7509851043467608611063b95cf01473
takehirodesuyo <takehiro.kt106@gmail.com>
2021-10-11 22:28:52 +0900

    スキルページ追加

--- resources/views/dog/index.blade.php
+++ resources/views/dog/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            スキル
         </h2>
     </x-slot>
 
@@ -9,37 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach($dogs as $dog)
-                        {{ $dog }}
-                    @endforeach
-
-                    
-
                     <table class="table-auto">
                         <thead>
                             <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Views</th>
+                            <th class="px-4 py-2">Title</th>
+                            <th class="px-4 py-2">Author</th>
+                            <th class="px-4 py-2">Views</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                            <td>Intro to CSS</td>
-                            <td>Adam</td>
-                            <td>858</td>
-                            </tr>
-                            <tr class="bg-emerald-200">
-                            <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-                            <td>Adam</td>
-                            <td>112</td>
-                            </tr>
-                            <tr>
-                            <td>Intro to JavaScript</td>
-                            <td>Chris</td>
-                            <td>1,280</td>
-                            </tr>
-                        </tbody>
+                        @foreach($dogs as $dog)
+                          <tr>
+                            <td class="border px-4 py-2">{{ $dog->id }}</td>
+                            <td class="border px-4 py-2">{{ $dog->dog_name }}</td>
+                            <td class="border px-4 py-2">{{ $dog->dog_status }}</td>
+                          </tr>
+                        @endforeach
+                    </tbody>
                     </table>
                 </div>
             </div>
